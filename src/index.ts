54aedda60ef3046f07de9c45f91fb7f4c429c610
megafredo <14902945+Megafredo@users.noreply.github.com>
2024-06-01 11:21:59 +0200

    [backend] Test pino

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,60 @@
 //~ Import modules
 import 'dotenv/config';
 import Pino from 'pino';
-const logger = Pino.pino();
+import pkg from 'pino-multi-stream';
+const { multistream } = pkg;
+// import { DateTime } from 'luxon';
+
+// const dateNow = DateTime.now()
+// const date = DateTime.fromISO(dateNow)
+
+
+const levels = {
+  emerg: 80,
+  alert: 70,
+  crit: 60,
+  error: 50,
+  warn: 40,
+  notice: 30,
+  info: 35,
+  debug: 10,
+};
+
+const streams = [
+  { stream: Pino.pino.destination('./src/app.log') }, // Fichier log
+  { stream: Pino.pino.destination(1)}, // Console avec pino-pretty (1 : stdout, 2: stderr)
+];
+
+const transport = multistream(streams);
+
+const logger = Pino.pino(
+  {
+    level: process.env.PINO_LOG_LEVEL || 'debug', // Niveau de log minimum
+    formatters: {
+      level: (label) => {
+        return { level: label.toUpperCase() };
+      },
+    },
+    timestamp: Pino.stdTimeFunctions.isoTime,
+    customLevels: levels,
+    useOnlyCustomLevels: true,
+  },
+  transport
+);
+
+const attribute = "attribute";
+
+// Exemple de log avec le niveau personnalisé 'emerg'
+logger.emerg({ attribute }, 'This is an emergency log');
+
+logger.info({ "attribute": attribute }, 'Info');
+// logger.fatal('fatal');
+// logger.error('error');
+// logger.warn('warn');
+// logger.info('info');
+// logger.debug('debug');
+// logger.trace('trace');
+// logger.notice('notice');
 
 import Fastify from 'fastify';
 const app = Fastify({ logger: false });
@@ -24,4 +77,6 @@ app.listen({ port: Number(PORT) }, (err) => {
     process.exit(1);
   }
   logger.info(`🚀\x1b[1;35m Launch server on http://localhost:${PORT}\x1b[0m`);
+
+
 });
